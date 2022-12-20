3af2fcdb0e00be24ba0faf1371080878f0605cdc
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2022-12-20 18:25:07 +0900

    add darkmode

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
   <div>
-    <p class="text-lg">hoge</p>
+    <h1>Color mode: {{ $colorMode.value }}</h1>
+    <button @click="$colorMode.preference = 'light'">Light</button>
+    <button @click="$colorMode.preference = 'dark'">Dark</button>
   </div>
 </template>
+
+<style>
+body {
+  background-color: #fff;
+  color: rgba(0,0,0,0.8);
+}
+.dark-mode body {
+  background-color: #091a28;
+  color: #ebf4f1;
+}
+</style>
