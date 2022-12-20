f96b7f1e140e491bca42bd9410906ae4a37cc3ef
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2022-12-20 20:27:27 +0900

    add darkmode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", '@nuxtjs/color-mode'],
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '',
+    storageKey: 'nuxt-color-mode'
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,7 @@
 <template>
   <div>
-    <h1>Color mode: {{ $colorMode.value }}</h1>
+    <h1 class="text-xl dark:text-xs">Color mode: {{ $colorMode.value }}</h1>
     <button @click="$colorMode.preference = 'light'">Light</button>
     <button @click="$colorMode.preference = 'dark'">Dark</button>
   </div>
 </template>
-
-<style>
-body {
-  background-color: #fff;
-  color: rgba(0,0,0,0.8);
-}
-.dark-mode body {
-  background-color: #091a28;
-  color: #ebf4f1;
-}
-</style>
